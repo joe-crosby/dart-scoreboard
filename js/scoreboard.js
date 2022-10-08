d42654eaff303694757ca8939e09a57edb06c101
= <jrcrosby1@gmail.com>
2022-10-08 13:18:59 -0400

    I adjusted a lot of css to make the UI more responsive.

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -6,44 +6,23 @@ const GAMES = {
 const GAMESCORES = {
   'Cricket': [
     null,
-    `<svg viewBox="0 0 40 40" stroke="black">
-      <path d="M9 6 L34 31 L30.5 34 L5.5 9.5 Z" />
+    `<svg height="100%" viewBox="0 0 40 40" stroke="black">
+      <path d="M30 6 L6 30 L9 34 L34 9 Z" />
       Sorry, your browser does not support inline SVG.
-    </svg>`,
-    `<svg viewBox="0 0 40 40" stroke="black">
-     <g>
-       <path d="M30 6 L6 30 L9 34 L34 9 Z" />
-       <path d="M9 6 L34 31 L30.5 34 L5.5 9.5 Z" />
-     </g>
+     </svg>`,
+    `<svg height="100%" viewBox="0 0 40 40" stroke="black">
+       <g>
+         <path d="M30 6 L6 30 L9 34 L34 9 Z" />
+         <path d="M9 6 L34 31 L30.5 34 L5.5 9.5 Z" />
+       </g>
      Sorry, your browser does not support inline SVG.
     </svg>`,
-    `<svg viewBox="0 0 15 15" stroke="black">
-      <circle cx="7.5" cy="7.5" r="6" stroke="black" stroke-width="2" fill="none" />
-    </svg>`,
+    `<svg height="100%" viewBox="0 0 15 15" stroke="black">
+       <circle cx="7.5" cy="7.5" r="6" stroke="black" stroke-width="2" fill="none" />
+     </svg>`,
   ],
   'Shanghi': null,
 }
-
-// TESTING svg
-/*
-<svg viewBox="0 0 40 40" stroke="black">
-  <path d="M9 6 L34 31 L30.5 34 L5.5 9.5 Z" />
-  Sorry, your browser does not support inline SVG.
-</svg>
-
- <svg viewBox="0 0 40 40" stroke="black">
-  <g>
-    <path d="M30 6 L6 30 L9 34 L34 9 Z" />
-    <path d="M9 6 L34 31 L30.5 34 L5.5 9.5 Z" />
-  </g>
-  Sorry, your browser does not support inline SVG.
- </svg>
-
-
-<svg viewBox="0 0 15 15" stroke="black">
-  <circle cx="7.5" cy="7.5" r="6" stroke="black" stroke-width="2" fill="none" />
-</svg>
-*/
 
 const GAMERULES = {
   'Cricket': {
