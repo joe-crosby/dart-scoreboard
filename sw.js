d1b397c481f4f049e82a063b610bc46118b8e4d3
Joseph Crosby <jrcrosby1@gmail.com>
2025-02-17 14:15:54 -0500

    I read more about service workers to understand more about how they work and fixed it.

--- sw.js
+++ sw.js
@@ -1,53 +1,96 @@
-const CACHE_NAME = "DartScoreboardOffline";
+const version = 1;
+const CACHE_NAME = `DartScoreboardOffline${version}`;
 
-var urlsToCache = [
-    'https://joe-crosby.github.io/dart-scoreboard/',
-    'https://joe-crosby.github.io/dart-scoreboard/images/dartboard.png',
-    'https://joe-crosby.github.io/dart-scoreboard/images/frame-background.png',
-    'https://joe-crosby.github.io/dart-scoreboard/css/messageboard.css',
-    'https://joe-crosby.github.io/dart-scoreboard/css/dartboard.css',
-    'https://joe-crosby.github.io/dart-scoreboard/css/scoreboard.css',
-    'https://joe-crosby.github.io/dart-scoreboard/js/games/dart-game.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/games/x01.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/games/around-the-world.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/games/cricket.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/games/shanghai.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/messageboard.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/player.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/dartboard.js',
-    'https://joe-crosby.github.io/dart-scoreboard/js/scoreboard.js'
+var cacheItemUrls = [
+    './',
+    './index.html',
+    './images/dartboard.png',
+    './images/frame-background.png',
+    './css/messageboard.css',
+    './css/dartboard.css',
+    './css/scoreboard.css',
+    './js/games/dart-game.js',
+    './js/games/x01.js',
+    './js/games/around-the-world.js',
+    './js/games/cricket.js',
+    './js/games/shanghai.js',
+    './js/messageboard.js',
+    './js/player.js',
+    './js/dartboard.js',
+    './js/scoreboard.js'
 ];
+
+const addResourceUrlsToCache = async (items) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(items).catch((err) => {
+    console.error(err);
+  });
+}
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.put(request, response);
+};
+
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cache = await caches.open(CACHE_NAME);
+  const cachedKeys = await cache.keys();
+  const cachesToDelete = cachedKeys.filter((key) => key != CACHE_NAME);
+  await Promise.all(cachesToDelete.map(deleteCache));
+}
 
 // Create the cache on install
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          return cache.addAll(urlsToCache);
-        })
-        .catch(function(err) {
-            console.error(err);
-        })
+      addResourceUrlsToCache(cacheItemUrls)
     );
 });
 
-// Network first, fall back to cache. This will keep the cache up to date.
-self.addEventListener('fetch', (event) => {
-  // Check if this is a navigation request
-  if (event.request.mode === 'navigate') {
-    // Open the cache
-    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-      // Go to the network first
-      return fetch(event.request.url).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
+self.addEventListener('activate', function(event) {
+    event.waitUntil(deleteOldCaches());
+});
 
-        return fetchedResponse;
-      }).catch(() => {
-        // If the network is unavailable, get
-        return cache.match(event.request.url);
-      });
-    }));
-  } else {
-    return;
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
   }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+      fallbackUrl: './index.html',
+    })
+  );
 });
